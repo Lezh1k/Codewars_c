14190a8be6be51b8f5c0ba433279fef9d6b4a0af
Lezh1k <lezh1k.vohrer@gmail.com>
2019-05-06 14:26:02 +0600

    Refactored wmm

--- src/main.c
+++ src/main.c
@@ -2,63 +2,402 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <float.h>
+#include <time.h>
+
 #include "skyscapers.h"
 
-#define SIZE 6
+//#define SIZE 6
 
-int check(int **solution, int (*expected)[SIZE]) {
-  bool result = false;
-  if (solution && expected) {
-    result = true;
-    for (int i = 0; i < SIZE; i++)
-      if (memcmp(solution[i], expected[i], SIZE * sizeof(int))) {
-        return false;
-      }
+//int check(int **solution, int (*expected)[SIZE]) {
+//  bool result = false;
+//  if (solution && expected) {
+//    result = true;
+//    for (int i = 0; i < SIZE; i++)
+//      if (memcmp(solution[i], expected[i], SIZE * sizeof(int))) {
+//        return false;
+//      }
+//  }
+//  return result;
+//}
+///////////////////////////////////////////////////////////
+
+//int main(int argc, char **argv) {
+//  (void)argc;
+//  (void)argv;
+//  setbuf(stdout, NULL);
+
+//  static const int clues[][SIZE*4] = {
+//    {
+//      3, 2, 2, 3, 2, 1,
+//      1, 2, 3, 3, 2, 2,
+//      5, 1, 2, 2, 4, 3,
+//      3, 2, 1, 2, 2, 4
+//    },
+//    {
+//      0, 0, 0, 2, 2, 0,
+//      0, 0, 0, 6, 3, 0,
+//      0, 4, 0, 0, 0, 0,
+//      4, 4, 0, 3, 0, 0
+//    }
+//  };
+
+//  static int expected[][SIZE][SIZE] = {
+//    { { 2, 1, 4, 3, 5, 6 },
+//      { 1, 6, 3, 2, 4, 5 },
+//      { 4, 3, 6, 5, 1, 2 },
+//      { 6, 5, 2, 1, 3, 4 },
+//      { 5, 4, 1, 6, 2, 3 },
+//      { 3, 2, 5, 4, 6, 1 } },
+//    { { 5, 6, 1, 4, 3, 2 },
+//      { 4, 1, 3, 2, 6, 5 },
+//      { 2, 3, 6, 1, 5, 4 },
+//      { 6, 5, 4, 3, 2, 1 },
+//      { 1, 2, 5, 6, 4, 3 },
+//      { 3, 4, 2, 5, 1, 6 } }
+//  };
+
+//  if (!check(SolvePuzzle(clues[0]), expected[0]))
+//    return -1;
+//  if (!check(SolvePuzzle(clues[1]), expected[1]))
+//    return -2;
+
+//  return 0;
+//}
+/////////////////////////////////////////////////////////
+
+#define pi 3.14159265359
+#define dtr pi/180.0
+
+#define a   6378.137
+#define b   6356.7523142
+#define re  6371.2
+
+#define a2  (a*a)
+#define b2  (b*b)
+#define c2  (a2-b2)
+#define a4  (a2*a2)
+#define b4  (b2*b2)
+#define c4  (a4-b4)
+
+//be careful with this.
+#define epoch 2015.0
+#define maxord 12
+
+static double sp[13];
+static double cp[13];
+static double pp[13];
+static double fn[13];
+static double fm[13];
+
+static double c[13][13];
+static double cd[13][13];
+static double k[13][13];
+static double snorm[13][13];
+
+static void initFromFile() {
+  int i, icomp, n, m, j, D1, D2;
+  double gnm, hnm, dgnm, dhnm, flnmj;
+  static char c_str[81], c_new[5];
+  FILE *wmmdat;
+  wmmdat = fopen("/home/lezh1k/SRC/WMM2015LegacyC/WMM.COF","r");
+  if (wmmdat == NULL) {
+    fprintf(stderr, "Error opening model file WMM.COF\n");
+    exit(1);
   }
-  return result;
+
+  /* INITIALIZE CONSTANTS */
+  sp[0] = 0.0;
+  cp[0] = pp[0] = 1.0;
+
+  /* READ WORLD MAGNETIC MODEL SPHERICAL HARMONIC COEFFICIENTS */
+  c[0][0] = 0.0;
+  cd[0][0] = 0.0;
+
+  fgets(c_str, 80, wmmdat); //read first line and do nothing!!!
+
+  while (true) {
+    if (fgets(c_str, 80, wmmdat) == NULL)
+      break;
+
+    /* CHECK FOR LAST LINE IN FILE */
+    for (i=0; i<4 && (c_str[i] != '\0'); i++) {
+      c_new[i] = c_str[i];
+      c_new[i+1] = '\0';
+    }
+
+    icomp = strcmp("9999", c_new);
+    if (icomp == 0)
+      break;
+
+    /* END OF FILE NOT ENCOUNTERED, GET VALUES */
+    sscanf(c_str,"%d%d%lf%lf%lf%lf",&n,&m,&gnm,&hnm,&dgnm,&dhnm);
+
+    if (n > maxord)
+      break;
+
+    if (m > n || m < 0.0) {
+      fprintf(stderr, "Corrupt record in model file WMM.COF\n");
+      exit(1);
+    }
+
+    if (m > n) continue;
+
+    c[m][n] = gnm;
+    cd[m][n] = dgnm;
+    if (m != 0) {
+      c[n][m-1] = hnm;
+      cd[n][m-1] = dhnm;
+    }
+  }
+  /* CONVERT SCHMIDT NORMALIZED GAUSS COEFFICIENTS TO UNNORMALIZED */
+
+  snorm[0][0] = 1.0;
+
+  fm[0] = 0.0;
+  for (n=1; n<=maxord; n++) {
+
+    snorm[0][n] = snorm[0][n-1] * (double)(2*n-1)/(double)n;
+    j = 2;
+
+    for (m=0, D1=1, D2=(n-m+D1)/D1; D2>0; D2--,m+=D1) {
+      k[m][n] = (double)(((n-1)*(n-1))-(m*m))/(double)((2*n-1)*(2*n-3));
+
+      if (m > 0) {
+        flnmj = (double)((n-m+1)*j) / (double)(n+m);
+        snorm[m][n] = snorm[m-1][n] * sqrt(flnmj);
+        j = 1;
+        c[n][m-1] *= snorm[m][n];
+        cd[n][m-1] *= snorm[m][n];
+      }
+      c[m][n] *= snorm[m][n];
+      cd[m][n] *= snorm[m][n];
+    }
+    fn[n] = (double)(n+1);
+    fm[n] = (double)n;
+  }
+  k[1][1] = 0.0;
+  fclose(wmmdat);
+  return;
+  /*************************************************************************/
 }
 ///////////////////////////////////////////////////////
 
-int main(int argc, char **argv) {
-  (void)argc;
-  (void)argv;
-  setbuf(stdout, NULL);
+static void computeMagneticFieldParameters(double alt,
+                                           double glat,
+                                           double glon,
+                                           double time,
+                                           double *dec,
+                                           double *dip,
+                                           double *ti,
+                                           double *gv)
+{
+  static int n, m, D3, D4;
+  double tc[13][13],
+      dt, rlon, rlat, srlon, srlat,
+      crlon, crlat, srlat2, crlat2,
+      q, q1, q2, ct, st, r2, r, d, ca, sa,
+      aor, ar, br, bt, bp, bpp,
+      par, temp1, temp2, parp, bx, by, bz, bh;
 
-  static const int clues[][SIZE*4] = {
-    {
-      3, 2, 2, 3, 2, 1,
-      1, 2, 3, 3, 2, 2,
-      5, 1, 2, 2, 4, 3,
-      3, 2, 1, 2, 2, 4
-    },
-    {
-      0, 0, 0, 2, 2, 0,
-      0, 0, 0, 6, 3, 0,
-      0, 4, 0, 0, 0, 0,
-      4, 4, 0, 3, 0, 0
-    }
-  };
+  double dp[13][13];
+  dp[0][0] = 0.0;
 
-  static int expected[][SIZE][SIZE] = {
-    { { 2, 1, 4, 3, 5, 6 },
-      { 1, 6, 3, 2, 4, 5 },
-      { 4, 3, 6, 5, 1, 2 },
-      { 6, 5, 2, 1, 3, 4 },
-      { 5, 4, 1, 6, 2, 3 },
-      { 3, 2, 5, 4, 6, 1 } },
-    { { 5, 6, 1, 4, 3, 2 },
-      { 4, 1, 3, 2, 6, 5 },
-      { 2, 3, 6, 1, 5, 4 },
-      { 6, 5, 4, 3, 2, 1 },
-      { 1, 2, 5, 6, 4, 3 },
-      { 3, 4, 2, 5, 1, 6 } }
-  };
+  dt = time - epoch;
+  rlon = glon*dtr;
+  rlat = glat*dtr;
+  srlon = sin(rlon);
+  srlat = sin(rlat);
+  crlon = cos(rlon);
+  crlat = cos(rlat);
+  srlat2 = srlat*srlat;
+  crlat2 = crlat*crlat;
+  sp[1] = srlon;
+  cp[1] = crlon;
 
-  if (!check(SolvePuzzle(clues[0]), expected[0]))
-    return -1;
-  if (!check(SolvePuzzle(clues[1]), expected[1]))
-    return -2;
+  /* CONVERT FROM GEODETIC COORDS. TO SPHERICAL COORDS. */
+  q = sqrt(a2 - c2*srlat2);
+  q1 = alt*q;
+  q2 = ((q1+a2) / (q1+b2)) * ((q1+a2) / (q1+b2));
+  ct = srlat / sqrt(q2*crlat2 + srlat2);
+  st = sqrt(1.0 - ct*ct);
+  r2 = (alt*alt) + 2.0*q1 + (a4 - c4*srlat2) / (q*q);
+  r = sqrt(r2);
+  d = sqrt(a2*crlat2 + b2*srlat2);
+  ca = (alt + d) / r;
+  sa = c2*crlat*srlat/(r*d);
 
-  return 0;
+  for (m=2; m<=maxord; m++) {
+    sp[m] = sp[1]*cp[m-1] + cp[1]*sp[m-1];
+    cp[m] = cp[1]*cp[m-1] - sp[1]*sp[m-1];
+  }
+
+  aor = re / r;
+  ar = aor*aor;
+  br = bt = bp = bpp = 0.0;
+  for (n=1; n<=maxord; n++) {
+    ar = ar*aor;
+    for (m=0,D3=1,D4=(n+m+D3)/D3; D4>0; D4--,m+=D3) {
+      /* COMPUTE UNNORMALIZED ASSOCIATED LEGENDRE POLYNOMIALS
+     AND DERIVATIVES VIA RECURSION RELATIONS */
+
+      do {
+        if (n == m) {
+          snorm[m][n] = st*snorm[m-1][n-1];
+          dp[m][n] = st*dp[m-1][n-1] + ct*snorm[m-1][n-1];
+          break;
+        }
+
+        if (n == 1 && m == 0) {
+          snorm[m][n] = ct*snorm[m][n-1];
+          dp[m][n] = ct*dp[m][n-1]-st*snorm[m][n-1];
+          break;
+        }
+
+        if (n > 1 && n != m) {
+          if (m > n-2)
+            snorm[m][n-2] = 0.0;
+          if (m > n-2)
+            dp[m][n-2] = 0.0;
+          snorm[m][n] = ct*snorm[m][n-1] - k[m][n]*snorm[m][n-2];
+          dp[m][n] = ct*dp[m][n-1] - st*snorm[m][n-1] - k[m][n]*dp[m][n-2];
+        }
+      } while (0);
+
+      /*
+      TIME ADJUST THE GAUSS COEFFICIENTS
+  */
+      tc[m][n] = c[m][n] + dt*cd[m][n];
+      if (m != 0)
+        tc[n][m-1] = c[n][m-1] + dt*cd[n][m-1];
+      /*
+      ACCUMULATE TERMS OF THE SPHERICAL HARMONIC EXPANSIONS
+  */
+      par = ar * snorm[m][n];
+      if (m == 0) {
+        temp1 = tc[m][n]*cp[m];
+        temp2 = tc[m][n]*sp[m];
+      } else {
+        temp1 = tc[m][n]*cp[m]+tc[n][m-1]*sp[m];
+        temp2 = tc[m][n]*sp[m]-tc[n][m-1]*cp[m];
+      }
+      bt = bt - ar * temp1 * dp[m][n];
+      bp += (fm[m] * temp2 * par);
+      br += (fn[n] * temp1 * par);
+
+      /*
+      SPECIAL CASE:  NORTH/SOUTH GEOGRAPHIC POLES
+  */
+      if (st == 0.0 && m == 1) {
+        if (n == 1) pp[n] = pp[n-1];
+        else pp[n] = ct*pp[n-1]-k[m][n]*pp[n-2];
+        parp = ar*pp[n];
+        bpp += (fm[m]*temp2*parp);
+      }
+    } //for (m=0,D3=1,D4=(n+m+D3)/D3; D4>0; D4--,m+=D3)
+  } //for (n=1; n<=maxord; n++)
+
+  if (st == 0.0) bp = bpp;
+  else bp /= st;
+
+  /*
+      ROTATE MAGNETIC VECTOR COMPONENTS FROM SPHERICAL TO
+      GEODETIC COORDINATES
+  */
+  bx = -bt*ca - br*sa;
+  by = bp;
+  bz = bt*sa - br*ca;
+
+  /*
+      COMPUTE DECLINATION (DEC), INCLINATION (DIP) AND
+      TOTAL INTENSITY (TI)
+  */
+
+  bh = sqrt((bx*bx)+(by*by));
+  *ti = sqrt((bh*bh)+(bz*bz));
+  *dec = atan2(by,bx)/dtr;
+  *dip = atan2(bz,bh)/dtr;
+  /*
+      COMPUTE MAGNETIC GRID VARIATION IF THE CURRENT
+      GEODETIC POSITION IS IN THE ARCTIC OR ANTARCTIC
+      (I.E. GLAT > +55 DEGREES OR GLAT < -55 DEGREES)
+
+      OTHERWISE, SET MAGNETIC GRID VARIATION TO -999.0
+  */
+  *gv = -999.0;
+  if (fabs(glat) >= 55.0) {
+    if (glat > 0.0 && glon >= 0.0) *gv = *dec-glon;
+    if (glat > 0.0 && glon < 0.0) *gv = *dec+fabs(glon);
+    if (glat < 0.0 && glon >= 0.0) *gv = *dec+glon;
+    if (glat < 0.0 && glon < 0.0) *gv = *dec-fabs(glon);
+    if (*gv > +180.0) *gv -= 360.0;
+    if (*gv < -180.0) *gv += 360.0;
+  }
+  return;
 }
-/////////////////////////////////////////////////////////
+/*************************************************************************/
+
+static void printArr1D(const char *descr, const double *arr, int len) {
+  const double *tmp = arr;
+  printf("%s: ", descr);
+  while (len--) {
+    printf("%f, ", *tmp++);
+  }
+  printf("\n----------\n");
+}
+///////////////////////////////////////////////////////
+
+static void printArr2D(const char *descr, const double mtx[][13], int w, int h) {
+  printf("%s: ", descr);
+  for (int y = 0; y < h; ++y) {
+    printf("\n");
+    for (int x = 0; x < w; ++x) {
+      printf("%f, ", mtx[y][x]);
+    }
+  }
+  printf("\n----------\n");
+}
+///////////////////////////////////////////////////////
+
+static void printArraysAfterInit() {
+  const double *arrays1D[] = {sp, cp, pp, fn, fm, NULL};
+  const char *descriptions1D[] = {"sp", "cp", "pp", "fn", "fm", NULL};
+  const double **tmpArr1D = arrays1D;
+  const char **descr1D = descriptions1D;
+
+  const char *descriptions2D[] = {"c", "cd", "k", NULL};
+  const char **descr2D = descriptions2D;
+
+  initFromFile();
+  for (; *tmpArr1D; ++tmpArr1D, ++descr1D) {
+    printArr1D(*descr1D, *tmpArr1D, 13);
+  }
+  ///////////////////////////////////////////////////////
+
+  printArr2D(*descr2D++, c, 13, 13);
+  printArr2D(*descr2D++, cd, 13, 13);
+  printArr2D(*descr2D++, k, 13, 13);
+}
+
+static bool fcmp(double l, double r) {
+  return fabs(l-r) <= DBL_EPSILON;
+}
+
+int main(int argc, char **argv) {
+  (void) argc;
+  (void) argv;
+  //declination, inclination, total intensity, grid variation
+  double dec, dip, ti, gv;
+  initFromFile();
+  computeMagneticFieldParameters(0.0, 42.879965, 74.617977, 2017.0, &dec, &dip, &ti, &gv);
+
+  printf("dec : %f\n", dec);
+  printf("dip : %f\n", dip);
+  printf("ti  : %f\n", ti);
+  printf("gv  : %f\n", gv);
+}
+
--- src/skyscapers.c
+++ src/skyscapers.c
@@ -4,29 +4,30 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include "skyscapers.h"
 
 #define SIZE 6
-#define FULL 0x7e
+#define FULL 0x3f
 
 typedef struct state {
   //main
-  int m_mtx[SIZE][SIZE];
-  int m_clues[SIZE*4];
+  int32_t m_mtx[SIZE][SIZE];
+  int32_t m_clues[SIZE*4];
 
   //aux
-  int m_col_mask[SIZE];
-  int m_row_mask[SIZE];
+  uint8_t m_col_mask[SIZE];
+  uint8_t m_row_mask[SIZE];
 
   //in this order we should solve
-  int m_row_indexes[SIZE];
-  int m_col_indexes[SIZE];
+  int32_t m_row_indexes[SIZE];
+  int32_t m_col_indexes[SIZE];
 } state_t;
 ///////////////////////////////////////////////////////
 
 typedef struct line {
-  int ix;
-  int clN;
+  int32_t ix;
+  int32_t clN;
 } line_t;
 
 static int line_cmp_desc(const void* l1, const void* l2) {
@@ -51,37 +52,44 @@ typedef enum direction {
 static state_t new_state(void);
 static void init_clues(state_t *st, const int *clues);
 
-static int row_n(const state_t *st, int r, direction_t d);
-static int col_n(const state_t *st, int c, direction_t d);
+static int row_n(const state_t *st, int32_t r, direction_t d);
+static int col_n(const state_t *st, int32_t c, direction_t d);
+static int row_n2(const state_t *st, int32_t r, direction_t d);
+static int col_n2(const state_t *st, int32_t c, direction_t d);
 
-static bool set_item(state_t *st, int r, int c, int it);
-static void clear_item(state_t *st, int r, int c);
+static bool set_item(state_t *st, int32_t r, int32_t c, int32_t it);
+static void clear_item(state_t *st, int32_t r, int32_t c);
 
 static bool state_is_final(const state_t *st);
-static bool check_clues(const state_t *st, int r, int c);
+static bool check_clues(const state_t *st, int32_t r, int32_t c);
 
-static bool row_is_ok(const state_t *st, int r);
-static bool col_is_ok(const state_t *st, int c);
+static bool row_is_ok(const state_t *st, int32_t r);
+static bool col_is_ok(const state_t *st, int32_t c);
+
+static void solve_edge_row(state_t *st, int32_t r);
+static void solve_edge_col(state_t *st, int32_t c);
+static void solve_edge(state_t* st);
+static bool solve_cleared(state_t *st, int32_t ri, int32_t ci, int32_t it);
 
 static void print_state(const state_t *st);
 
 state_t new_state() {
-  int r;
+  int32_t r;
   state_t st;
   for (r = 0; r < SIZE; ++r)
-    memset(st.m_mtx[r], 0, sizeof(int)*SIZE);
-  memset(st.m_col_mask, 0, sizeof (int)*SIZE);
-  memset(st.m_row_mask, 0, sizeof (int)*SIZE);
+    memset(st.m_mtx[r], 0, sizeof(int32_t)*SIZE);
+  memset(st.m_col_mask, 0, sizeof (int32_t)*SIZE);
+  memset(st.m_row_mask, 0, sizeof (int32_t)*SIZE);
   return st;
 }
 ///////////////////////////////////////////////////////
 
 void init_clues(state_t *st,
-               const int *clues) {
+               const int32_t *clues) {
   line_t rlines[SIZE];
   line_t clines[SIZE];
-  int i;
-  memcpy(st->m_clues, clues, sizeof(int)*SIZE*4);
+  int32_t i;
+  memcpy(st->m_clues, clues, sizeof(int32_t)*SIZE*4);
 
   for (i = 0; i < SIZE; ++i) {
     clines[i].ix = rlines[i].ix = i;
@@ -103,19 +111,19 @@ void init_clues(state_t *st,
 }
 ///////////////////////////////////////////////////////
 
-static const int dir_steps[4] = {-1, 1, -1, 1};
-static const int dir_starts[4] = {SIZE-1, 0, SIZE-1, 0};
+static const int32_t dir_steps[4] = {-1, 1, -1, 1};
+static const int32_t dir_starts[4] = {SIZE-1, 0, SIZE-1, 0};
 
-int row_n(const state_t *st,
-          int r,
+int32_t row_n(const state_t *st,
+          int32_t r,
           direction_t d) {
   assert(st);
   assert(r >= 0 && r < SIZE);
   assert(d == left || d == right);
-  int cs = dir_starts[d];
-  int ce = cs + (SIZE * dir_steps[d]);
-  int max = st->m_mtx[r][cs];
-  int n = max != 0 ? 1 : 0; //we can see first one
+  int32_t cs = dir_starts[d];
+  int32_t ce = cs + (SIZE * dir_steps[d]);
+  int32_t max = st->m_mtx[r][cs];
+  int32_t n = max != 0 ? 1 : 0; //we can see first one
   cs += dir_steps[d];
   for(; cs != ce; cs += dir_steps[d]) {
     if (st->m_mtx[r][cs] <= max)
@@ -127,16 +135,42 @@ int row_n(const state_t *st,
 }
 ///////////////////////////////////////////////////////
 
-int col_n(const state_t *st,
-          int c,
+int32_t row_n2(const state_t *st,
+           int32_t r,
+           direction_t d) {
+  assert(st);
+  assert(r >= 0 && r < SIZE);
+  assert(d == left || d == right);
+  int32_t cs = dir_starts[d];
+  int32_t ce = cs + (SIZE * dir_steps[d]);
+  int32_t max = st->m_mtx[r][cs];
+  int32_t n = max != 0 ? 1 : 0; //we can see first one
+
+  if (max == 0)
+    return 0; //we can't predict on this step.
+
+  cs += dir_steps[d];
+  for(; cs != ce; cs += dir_steps[d]) {
+    if (st->m_mtx[r][cs] == 0) break;
+    if (st->m_mtx[r][cs] <= max)
+      continue;
+    ++n;
+    max = st->m_mtx[r][cs];
+  }
+  return n;
+}
+///////////////////////////////////////////////////////
+
+int32_t col_n(const state_t *st,
+          int32_t c,
           direction_t d) {
   assert(st);
   assert(c >= 0 && c < SIZE);
   assert(d == up|| d == down);
-  int rs = dir_starts[d];
-  int re = rs + (SIZE * dir_steps[d]);
-  int max = st->m_mtx[rs][c];
-  int n = max != 0 ? 1 : 0; //we can see first one
+  int32_t rs = dir_starts[d];
+  int32_t re = rs + (SIZE * dir_steps[d]);
+  int32_t max = st->m_mtx[rs][c];
+  int32_t n = max != 0 ? 1 : 0; //we can see first one
   rs += dir_steps[d];
   for(; rs != re; rs += dir_steps[d]) {
     if (st->m_mtx[rs][c] <= max)
@@ -148,15 +182,40 @@ int col_n(const state_t *st,
 }
 ///////////////////////////////////////////////////////
 
+int32_t col_n2(const state_t *st,
+           int32_t c,
+           direction_t d) {
+  assert(st);
+  assert(c >= 0 && c < SIZE);
+  assert(d == up|| d == down);
+  int32_t rs = dir_starts[d];
+  int32_t re = rs + (SIZE * dir_steps[d]);
+  int32_t max = st->m_mtx[rs][c];
+  int32_t n = max != 0 ? 1 : 0; //we can see first one
+  if (max == 0)
+    return 0;
+
+  rs += dir_steps[d];
+  for(; rs != re; rs += dir_steps[d]) {
+    if (st->m_mtx[rs][c] == 0) break;
+    if (st->m_mtx[rs][c] <= max)
+      continue;
+    ++n;
+    max = st->m_mtx[rs][c];
+  }
+  return n;
+}
+///////////////////////////////////////////////////////
+
 bool set_item(state_t *st,
-              int r,
-              int c,
-              int it) {
+              int32_t r,
+              int32_t c,
+              int32_t it) {
   assert(c < SIZE && c >= 0);
   assert(r < SIZE && r >= 0);
   assert(it >= 1 && it <= SIZE);
   assert(st);
-
+  --it;
   if (st->m_col_mask[c] & (1 << it))
     return false;
   if (st->m_row_mask[r] & (1 << it))
@@ -164,26 +223,26 @@ bool set_item(state_t *st,
 
   st->m_col_mask[c] |= (1 << it);
   st->m_row_mask[r] |= (1 << it);
-  st->m_mtx[r][c] = it;
+  st->m_mtx[r][c] = ++it;
   return true;
 }
 ///////////////////////////////////////////////////////
 
 void clear_item(state_t *st,
-                int r,
-                int c) {
+                int32_t r,
+                int32_t c) {
   assert(c < SIZE && c >= 0);
   assert(r < SIZE && r >= 0);
   assert(st);
-  st->m_col_mask[c] &= ~(1 << st->m_mtx[r][c]);
-  st->m_row_mask[r] &= ~(1 << st->m_mtx[r][c]);
+  st->m_col_mask[c] &= ~(1 << (st->m_mtx[r][c]-1));
+  st->m_row_mask[r] &= ~(1 << (st->m_mtx[r][c]-1));
   st->m_mtx[r][c] = 0;
 }
 ///////////////////////////////////////////////////////
 
 bool row_is_ok(const state_t *st,
-               int r) {
-  int q;
+               int32_t r) {
+  int32_t q;
   //row l->r
   if ((q = st->m_clues[SIZE * 4 - r - 1])) {
     if (row_n(st, r, right) != q)
@@ -199,8 +258,8 @@ bool row_is_ok(const state_t *st,
 ///////////////////////////////////////////////////////
 
 bool col_is_ok(const state_t *st,
-               int c) {
-  int q;
+               int32_t c) {
+  int32_t q;
   //col u->d
   if ((q = st->m_clues[c])) {
     if (col_n(st, c, down) != q)
@@ -216,7 +275,7 @@ bool col_is_ok(const state_t *st,
 ///////////////////////////////////////////////////////
 
 bool state_is_final(const state_t *st) {
-  int qi;
+  int32_t qi;
   for (qi=0; qi < SIZE; ++qi) {
     if (!row_is_ok(st, qi))
       return false;
@@ -228,7 +287,7 @@ bool state_is_final(const state_t *st) {
 ///////////////////////////////////////////////////////
 
 void print_state(const state_t *st) {
-  int r, c, qi;
+  int32_t r, c, qi;
   printf("\n");
 
   for (qi = 0; qi < SIZE; ++qi)
@@ -250,12 +309,29 @@ void print_state(const state_t *st) {
 }
 ///////////////////////////////////////////////////////
 
-bool check_clues(const state_t *st, int r, int c) {
-  int clue, cnt;
+static int32_t trailing_zeros(uint8_t v) {
+  int32_t c = 8; // c will be the number of zero bits on the right
+  v &= (uint8_t)(-((int8_t)v)); //the least significant bit in v
+  if (v) c--;
+  if (v & 0x0f) c -= 4;
+  if (v & 0x33) c -= 2;
+  if (v & 0x55) c -= 1;
+  return c;
+}
+///////////////////////////////////////////////////////
+
+bool check_clues(const state_t *st, int32_t r, int32_t c) {
+  int32_t clue, cnt;
+
   if (st->m_row_mask[r] == FULL) { //check full row
     if ((clue = st->m_clues[SIZE*4 - 1 - r]) && (cnt = row_n(st, r, right)) != clue)
       return false;
     if ((clue = st->m_clues[SIZE + r]) && (cnt = row_n(st, r, left)) != clue)
+      return false;
+  } else {
+    if ((clue = st->m_clues[SIZE*4 - 1 - r]) && (cnt = row_n2(st, r, right) > clue))
+      return false;
+    if ((clue = st->m_clues[SIZE + r]) && (cnt = row_n2(st, r, left)) > clue)
       return false;
   }
 
@@ -264,90 +340,90 @@ bool check_clues(const state_t *st, int r, int c) {
       return false;
     if ((clue = st->m_clues[SIZE*3 - 1 - c]) && (cnt = col_n(st, c, up)) != clue)
       return false;
+  } else {
+    if ((clue = st->m_clues[c]) && (cnt = col_n2(st, c, down)) > clue)
+      return false;
+    if ((clue = st->m_clues[SIZE*3 - 1 - c]) && (cnt = col_n2(st, c, up)) > clue)
+      return false;
   }
   return true;
 }
 ///////////////////////////////////////////////////////
 
-static void solve_edge_row(state_t *st,
-                           int r,
-                           int d,
-                           int clue) {
+void solve_edge_row(state_t *st,
+                    int32_t r) {
   assert(st);
   assert(r >= 0 && r < SIZE);
-  assert(d == left || d == right);
-  assert(clue == SIZE || clue == 1);
-  int cs = dir_starts[d];
-  int ce = cs + (SIZE * dir_steps[d]);
-  int v, vs;
+  int32_t cL, cR, i;
+  cL = st->m_clues[SIZE*4 - 1 - r];
+  cR = st->m_clues[SIZE + r];
 
-  if (clue == 1) {
-    set_item(st, r, cs, SIZE);
-    return;
+  if (cL + cR == SIZE + 1) {
+    set_item(st, r, cL-1, SIZE);
   }
-  v = vs = 1;
-  for(; cs != ce; cs += dir_steps[d], v += vs)
-    set_item(st, r, cs, v);
+
+  if (cL == SIZE) {
+    for (i = 0; i < SIZE; ++i)
+      set_item(st, r, i, i+1);
+  }
+
+  if (cR == SIZE) {
+    for (i = 0; i < SIZE; ++i)
+      set_item(st, r, i, SIZE-i);
+  }
+
+  if (cL == 1)
+    set_item(st, r, 0, SIZE);
+  if (cR == 1)
+    set_item(st, r, SIZE-1, SIZE);
 }
 ///////////////////////////////////////////////////////
 
-static void solve_edge_col(state_t *st,
-                           int c,
-                           int d,
-                           int clue) {
+void solve_edge_col(state_t *st,
+                    int32_t c) {
   assert(st);
   assert(c >= 0 && c < SIZE);
-  assert(d == up|| d == down);
-  assert(clue == 1 || clue == SIZE);
-  int rs = dir_starts[d];
-  int re = rs + (SIZE * dir_steps[d]);
-  int v, vs;
+  int32_t cU, cD, i;
+  cU = st->m_clues[c];
+  cD = st->m_clues[SIZE*3 - 1 - c];
 
-  if (clue == 1) {
-    set_item(st, rs, c, SIZE);
-    return;
+  if (cU + cD == SIZE + 1) {
+    set_item(st, cU-1, c, SIZE);
   }
-  v = vs = 1;
-  for(; rs != re; rs += dir_steps[d], v += vs)
-    set_item(st, rs, c, v);
+
+  if (cU == SIZE) {
+    for (i = 0; i < SIZE; ++i)
+      set_item(st, i, c, i+1);
+  }
+
+  if (cD == SIZE) {
+    for (i = 0; i < SIZE; ++i)
+      set_item(st, i, c, SIZE-1);
+  }
+
+  if (cU == 1)
+    set_item(st, 0, c, SIZE);
+  if (cD == 1)
+    set_item(st, SIZE-1, c, SIZE);
 }
 ///////////////////////////////////////////////////////
 
-static void solve_edge(state_t* st) {
-  int qi;
-  //top clues
+void solve_edge(state_t* st) {
+  int32_t qi;
   for (qi = 0; qi < SIZE; ++qi) {
-    if (st->m_clues[qi] != 1 && st->m_clues[qi] != SIZE)
-      continue;
-    solve_edge_col(st, qi, down, st->m_clues[qi]);
-  }
-  //right clues
-  for (qi = SIZE; qi < SIZE*2; ++qi) {
-    if (st->m_clues[qi] != 1 && st->m_clues[qi] != SIZE)
-      continue;
-    solve_edge_row(st, qi-SIZE, left, st->m_clues[qi]);
-  }
-  //bottom clues
-  for (qi = SIZE*2; qi < SIZE*3; ++qi) {
-    if (st->m_clues[qi] != 1 && st->m_clues[qi] != SIZE)
-      continue;
-    solve_edge_col(st, SIZE*3 - 1 - qi, up, st->m_clues[qi]);
-  }
-  //left clues
-  for (qi = SIZE*3; qi < SIZE*4; ++qi) {
-    if (st->m_clues[qi] != 1 && st->m_clues[qi] != SIZE)
-      continue;
-    solve_edge_row(st, SIZE*4 - 1 - qi, right, st->m_clues[qi]);
+    solve_edge_row(st, qi);
+    solve_edge_col(st, qi);
   }
 }
 ///////////////////////////////////////////////////////
 
 //this will skip not 0 items.
-static bool solve_cleared(state_t *st,
-                          int ri,
-                          int ci,
-                          int it) {
-  int r, c;
+bool
+solve_cleared(state_t *st,
+                   int32_t ri,
+                   int32_t ci,
+                   int32_t it) {
+  int32_t r, c;
   if (ci == SIZE) {
     ++ri; ci = 0;
   }
@@ -355,7 +431,7 @@ static bool solve_cleared(state_t *st,
   c = st->m_col_indexes[ci];
 
   if (ri == SIZE)
-    return state_is_final(st);  
+    return state_is_final(st);
   if (it == SIZE+1)
     return false;
   if (st->m_mtx[r][c])
@@ -363,7 +439,7 @@ static bool solve_cleared(state_t *st,
 
   do {
     if (!set_item(st, r, c, it))
-      break;    
+      break;
     if (!check_clues(st, r, c))
       break;
     if (solve_cleared(st, ri, ci+1, 1))
@@ -381,20 +457,20 @@ static bool solve_smart(state_t *st) {
 }
 ///////////////////////////////////////////////////////
 
-int **SolvePuzzle(const int *clues) {
-  int **result = malloc(SIZE * sizeof (int*));
+int32_t **SolvePuzzle(const int32_t *clues) {
+  int32_t **result = malloc(SIZE * sizeof (int32_t*));
   bool solved = false;
   state_t st = new_state();
-  int r;
+  int32_t r;
   for (r = 0; r < SIZE; ++r)
-    result[r] = malloc(SIZE * sizeof(int));
+    result[r] = malloc(SIZE * sizeof(int32_t));
 
-  init_clues(&st, clues);  
+  init_clues(&st, clues);
   solved = solve_smart(&st);
   if (solved) {
     print_state(&st);
     for (r = 0; r < SIZE; ++r)
-      memcpy(result[r], st.m_mtx[r], SIZE*sizeof (int));
+      memcpy(result[r], st.m_mtx[r], SIZE*sizeof (int32_t));
     return result;
   }
   return NULL;
