e2292138fa5327e2d49c6387a98bf6db5cd1b4b6
Oleg Katkov <lezh1k.vohrer@gmail.com>
2025-05-15 17:34:22 +0600

    Modified gitignore + fixed comments in cat shellcode

--- asm/cat/src/cat.asm
+++ asm/cat/src/cat.asm
@@ -9,21 +9,21 @@ _start:
   push rdi
   mov rdi, rsp
 
-  ; argv[0] = "////////flag.txt"
+  ; argv[0] = "/flg.txt"
   push rax
   mov rsi, "/flg.txt"
   push rsi
   mov rsi, rsp 
 
   ; argv = [rdi, rsi, NULL]
-  push rax                      ; NULL
-  push rsi                      ; "////////flag.txt"
-  push rdi                      ; "/bin/cat"
-  mov rsi, rsp                  ; rsi -> argv array
+  push rax      ; NULL
+  push rsi      ; "////////flag.txt"
+  push rdi      ; "/bin/cat"
+  mov rsi, rsp  ; rsi -> argv array
 
   ; envp = NULL
   xor rdx, rdx  ; set env to NULL
-  mov al, 59 ; execve
+  mov al, 59    ; execve
   syscall
   ret
 ; ----------------------------------------
