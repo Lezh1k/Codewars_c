bc210c5617d1a5d754608cde5741758c0489af01
Oleg Katkov <lezh1k.vohrer@gmail.com>
2025-12-12 20:43:14 +0600

    prime_stream solution

--- src/main.c
+++ src/main.c
@@ -1,13 +1,18 @@
-#include "inc_dec_sequences_count.h"
 #include <stdalign.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <unistd.h>
+#include "prime_stream_simple.h"
 
 int main(int argc, char *argv[]) {
   (void)argc;
   (void)argv;
-  uint32_t N = 3;
-  printf("%lu\n", total_inc_dec(N));
+  struct prime_stream *ps = prime_stream_new();
+  for (uint32_t i = 0; i < 1000000; ++i) {
+    printf("%u\t", prime_stream_next(ps));
+    if ((i + 1) % 10 == 0)
+      printf("\n");
+  }
 }
 //////////////////////////////////////////////////////////////
